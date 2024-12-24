99072c5b8ec3177c475fe9552eed24cac3c32ee7
j0hnnym1 <johnnymichos@gmail.com>
2024-12-24 09:40:24 +0200

    front end structure

---
+++ frontend/src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import { QueryClientProvider } from '@tanstack/react-query';
+import './index.css';
+import App from './App';
+import queryClient from './queryClient';
+
+ReactDOM.render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <App />
+    </QueryClientProvider>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
